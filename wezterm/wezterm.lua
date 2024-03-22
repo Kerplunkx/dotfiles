f5b5003a2e9257979e176610da5c5c21b4109f94
Guillermo Moreira <guillermomoreira01@gmail.com>
2024-03-22 16:36:03 -0500

    deleted pycache 🤡

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,16 +1,11 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
 config.color_scheme = 'Gruber (base16)'
---config.color_scheme = 'SpaceGray'
 config.use_fancy_tab_bar = false
 config.font_size = 11
+config.font = wezterm.font 'JetBrains Mono'
 
 config.window_padding = {
   left = 0,
@@ -19,5 +14,4 @@ config.window_padding = {
   bottom = 0,
 }
 
--- and finally, return the configuration to wezterm
 return config
