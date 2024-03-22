e87d2623bb927319381e313954cbd638cc26f5fd
Guillermo Moreira <guillermomoreira01@gmail.com>
2024-03-22 16:04:17 -0500

    first commit

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,23 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Gruber (base16)'
+--config.color_scheme = 'SpaceGray'
+config.use_fancy_tab_bar = false
+config.font_size = 11
+
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+-- and finally, return the configuration to wezterm
+return config
