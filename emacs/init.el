ded18c252a1de0f9ba4c3d9328f2ccb0cddfa4ee
Guillermo Moreira <guillermomoreira01@gmail.com>
2024-05-26 09:36:45 -0500

    more emacs conf

--- emacs/init.el
+++ emacs/init.el
@@ -1,7 +1,11 @@
-;;(load-theme 'wombat)
 (menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 (global-display-line-numbers-mode 1)
+
 (setq make-backup-files nil)
+
+(set-frame-font "Iosevka Nerd Font 13" nil t)
 
 (load "~/.config/emacs/packages.el")
 (load-theme 'zenburn t)
